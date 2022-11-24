2ab085c13b94d4d02f2b8dcaf88d0ebecd611cad
Vince Kruger <vincekruger@gmail.com>
2022-11-24 13:02:32 +0100

    Update entrypoint.sh
    changed to env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ STATUS_ARGS=$1
 PATHSPEC=$2
 
 function check() {
-  git config --global --add safe.directory /github/workspace;
+  
+  git config --global --add safe.directory $GITHUB_WORKSPACE;
   
   if [[ -z "$(git status --porcelain $STATUS_ARGS $PATHSPEC)" ]];
   then
